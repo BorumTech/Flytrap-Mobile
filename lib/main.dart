263caed2ea7b3976983a85836ff05c517cc52b1f
Varun <31084854+varunsingh87@users.noreply.github.com>
2022-07-02 13:10:27 -0400

    Set up navigation, rooms, join room buttons, icons * Icons: Add Flytrap logo for icon using flutter_launch_icons plugin package * Navigation: BottomNavigationBar (nothing fancy yet, just index and subroutes use Navigation.push) * Rooms: List of public rooms or joined rooms (haven't decided this yet), create button, join button * Join Room - enter code, join as performer (seat selection), join as listener (rickroll)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,118 +12,266 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        title: 'Flytrap Audio',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const CurrentPage());
+  }
+}
+
+class _CurrentPageState extends State<CurrentPage> {
+  int _currentIndex = 0;
+  final screens = const [FilesPage(), MyRoomsPage(), SettingsPage()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: IndexedStack(index: _currentIndex, children: screens),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          showUnselectedLabels: false,
+          currentIndex: _currentIndex,
+          onTap: (index) => setState(() => _currentIndex = index),
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.folder),
+              label: 'Files',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.live_tv),
+              label: 'Live Broadcasts',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+        ));
+  }
+}
+
+class CurrentPage extends StatefulWidget {
+  const CurrentPage({Key? key}) : super(key: key);
+
+  @override
+  State<CurrentPage> createState() => _CurrentPageState();
+}
+
+class FilesPage extends StatefulWidget {
+  const FilesPage({Key? key}) : super(key: key);
+
+  @override
+  State<FilesPage> createState() => _FilesPageState();
+}
+
+class _FilesPageState extends State<FilesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Flytrap Audio",
+          textAlign: TextAlign.center,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyRoomsPage extends StatelessWidget {
+  const MyRoomsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar(title: const Text("Rooms")),
+      body: Column(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Create", style: TextStyle(fontSize: 18)),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const JoinRoom()));
+                    },
+                    child: const Text("Join", style: TextStyle(fontSize: 18)))
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                roomPreview(1),
+                roomPreview(2),
+                roomPreview(3),
+                roomPreview(4),
+                roomPreview(5),
+                roomPreview(6),
+                roomPreview(7)
+              ],
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+    );
+  }
+}
+
+Widget roomPreview(int index) => Container(
+      decoration: BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+      child: TextButton(
+        onPressed: () {},
+        child: Text("Room $index"),
+      ),
+    );
+
+class JoinRoom extends StatelessWidget {
+  const JoinRoom({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Join Room"),
+        ),
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1.0,
+                ),
+              ),
+              child: const TextField(
+                style: TextStyle(fontSize: 36),
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    labelText: "Room Code",
+                    floatingLabelAlignment: FloatingLabelAlignment.center,
+                    alignLabelWithHint: true,
+                    contentPadding: EdgeInsets.all(6)),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SeatSelectionPage()));
+              },
+              child: const Text("Join Room as Performer"),
             ),
+            ElevatedButton(
+                onPressed: () async {
+                  final Uri url =
+                      Uri.parse("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
+                  if (!await launchUrl(url)) throw 'Could not launch $url';
+                },
+                child: const Text("Join Room as Listener")),
           ],
         ),
+      );
+}
+
+class SeatSelectionPage extends StatefulWidget {
+  const SeatSelectionPage({Key? key}) : super(key: key);
+
+  @override
+  State<SeatSelectionPage> createState() => _SeatSelectionPageState();
+}
+
+class _SeatSelectionPageState extends State<SeatSelectionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Seat Selection"),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.folder),
-            label: 'Files',
+      body: ListView(
+        padding: const EdgeInsets.all(10.0),
+        scrollDirection: Axis.vertical,
+        children: [
+          const Text(
+              "Select a seat. Stronger and more essential players tend to seat first.",
+              style: TextStyle(fontSize: 24)),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            alignment: Alignment.bottomCenter,
+            child: Wrap(
+              direction: Axis.horizontal,
+              spacing: 50.0,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              runSpacing: 40.0,
+              children: [
+                seat(1),
+                seat(2),
+                seat(3),
+                seat(4),
+                seat(5),
+                seat(6),
+                seat(7),
+                seat(8),
+                seat(9),
+                seat(10),
+                seat(11),
+                seat(12),
+              ],
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.live_tv),
-            label: 'Live Broadcasts',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
+        ],
+      ),
+    );
+  }
+}
+
+Widget seat(int index) => Container(
+      width: 100,
+      height: 100,
+      color: Colors.green,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+      ),
+      body: Column(
+        children: [
+          const Text("Settings"),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Logout"),
           ),
         ],
       ),
